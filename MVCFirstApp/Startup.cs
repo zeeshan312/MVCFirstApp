f15f8c7ac23b50ddce647765edaa24b79564e8ae
whinstone1 <zeemurrawat@hotmail.com>
2018-03-06 13:06:50 +0500

    Add project files.

---
+++ MVCFirstApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFirstApp.Startup))]
+namespace MVCFirstApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
